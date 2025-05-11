822d81a830fe8aaa1268376118a120a2caf39e94
krzysztofrz <34165654+krzysztofrz@users.noreply.github.com>
2025-05-11 14:37:07 +0200

    loader added

--- src/App.tsx
+++ src/App.tsx
@@ -3,27 +3,34 @@ import { Debtor } from "./types/Debtor";
 import { getTopDebts, getFilteredDebts } from "./api/debts";
 import { SearchBar } from "./components/SearchBar/SearchBar";
 import { DebtorTable } from "./components/DebtorTable/DebtorTable";
+import { Loader } from "./components/Loader/Loader";
 
 const App = () => {
 	const [debts, setDebts] = useState<Debtor[]>([]);
 	const [query, setQuery] = useState("");
-
-	const fetchTop = async () => {
-		const data = await getTopDebts();
-		setDebts(data);
-	};
+	const [isLoading, setIsLoading] = useState(false);
 
 	const handleSearch = async () => {
 		try {
+			setIsLoading(true);
 			if (query.length >= 3) {
 				const results = await getFilteredDebts(query);
 				setDebts(results);
 			} else {
-				await fetchTop(); // fallback do TOP10
+				await fetchTop();
 			}
 		} catch (err) {
 			alert("Wprowadź co najmniej 3 znaki lub spróbuj ponownie.");
+		} finally {
+			setIsLoading(false);
 		}
+	};
+
+	const fetchTop = async () => {
+		setIsLoading(true);
+		const data = await getTopDebts();
+		setDebts(data);
+		setIsLoading(false);
 	};
 
 	useEffect(() => {
@@ -33,7 +40,7 @@ const App = () => {
 	return (
 		<div>
 			<SearchBar value={query} onChange={setQuery} onSearch={handleSearch} />
-			<DebtorTable data={debts} />
+			{isLoading ? <Loader /> : <DebtorTable data={debts} />}
 		</div>
 	);
 };
